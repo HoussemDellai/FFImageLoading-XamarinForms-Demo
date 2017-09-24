d439ae976ed9ae05b6beb12289ae2c0303f91133
Houssem Dellai <houssem.dellai@live.com>
2017-09-24 12:30:06 +0200

    Add project files.

---
+++ FfImageLoadingApp/FfImageLoadingApp/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace FfImageLoadingApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //Gif.Source =  
+            //    ImageSource.FromUri(new Uri("https://media.giphy.com/media/l0Hlyi4ZMJI9MpFUQ/giphy.gif"));
+        }
+    }
+}
